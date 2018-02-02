4c3915f672f4bc5d70a078c9fbe911081b47d567
Guillermo Calderon <guillermojose.calderon@gmail.com>
2018-02-02 00:41:52 +0000

    Corrected refresh UI bug

--- src/app/climate-chart/climate-chart.component.ts
+++ src/app/climate-chart/climate-chart.component.ts
@@ -14,31 +14,33 @@ export class ClimateChartComponent implements  AfterViewInit{
   MONTHS =["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   region_list = ALL_REG_LIST;
   selected_region: string;
+  maxT=[];
+  minT=[];
+  meanT=[];
   years = [];
   selected_year: string;
   constructor() { }
 
   ngAfterViewInit(){
-    this.refreshClimateChart([],[],[])
-
+    this.refreshClimateChart();
   }
 
   onRegionChange(): void{
     let temp_region =this.all_regions_detail[this.selected_region];
     this.years = Object.keys(temp_region);
-    this.refreshClimateChart([],[],[])
+    this.refreshClimateChart();
   }
   onYearChange(): void{
     let temp_region =this.all_regions_detail[this.selected_region];
     let t_values=temp_region[this.selected_year];
     console.log(t_values);
-    let maxT = t_values['maxT'];
-    let minT = t_values['minT'];
-    let meanT = t_values['meanT'];
-    this.refreshClimateChart(maxT, minT, meanT);
+    this.maxT = t_values['maxT'];
+    this.minT = t_values['minT'];
+    this.meanT = t_values['meanT'];
+    this.refreshClimateChart();
   }
 
-  refreshClimateChart(maxT, minT, meanT){
+  refreshClimateChart(){
     //start test code
     var time_series = this.MONTHS;
     var chart_options = chart_options;
@@ -51,21 +53,21 @@ export class ClimateChartComponent implements  AfterViewInit{
           fill: true,
           backgroundColor: "rgba(0, 191, 255, 0.2)",
           borderColor: "#29b6f6",
-          data: minT
+          data: this.minT
         },
         {
           label: "Mean",
           fill:false,
           backgroundColor: "#424242",
           borderColor: "#424242",
-          data: meanT
+          data: this.meanT
         },
         {
           label: "Max",
           fill: true,
           backgroundColor: "rgba(255, 209, 128,0.2)",
           borderColor: "#ffd180",
-          data: maxT
+          data: this.maxT
         }]
     };
 
